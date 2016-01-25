2700791bfb11162440c1211319a3f56faa729690
youzi <youzigege2048@qq.com>
2016-01-25 22:24:29 +0800

    第二次

--- app/src/main/java/com/example/youzi/myapplication/test.java
+++ app/src/main/java/com/example/youzi/myapplication/test.java
@@ -4,5 +4,8 @@ package com.example.youzi.myapplication;
  * Created by youzi on 2016/1/25.
  */
 public class test {
-
+    public static void main(String[] argc)
+    {
+        System.out.printf("123");
+    }
 }
